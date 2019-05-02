3d1e8f6eef8a0d80ee54fc3973d9812ab817bfaa
Mridhula.k <46248002+mridhulak99@users.noreply.github.com>
2019-05-02 22:34:31 +0530

    Add files via upload

---
+++ components/js/navbar.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    console.log("heoolo:");
+    $('.dropdown-menu a.dropdown-toggle').on('click', function (e) {
+        if (!$(this).next().hasClass('show')) {
+            $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+        }
+        var $subMenu = $(this).next(".dropdown-menu");
+        $subMenu.toggleClass('show');
+
+
+        $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function (e) {
+            $('.dropdown-submenu .show').removeClass("show");
+        });
+
+        console.log("called");
+        return false;
+    });
+  
+   
+  
+});
